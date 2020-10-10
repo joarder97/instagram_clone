2504525fcacdbbc93caeed221ee1e192b68c032f
joarder97 <masudul.pallab99@gmail,com>
2020-10-10 22:22:52 +0600

    style added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,18 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-4">
+            <img src="https://instagram.fdac31-1.fna.fbcdn.net/v/t51.2885-19/s320x320/118171719_774838613351913_2225032456729400735_n.jpg?_nc_ht=instagram.fdac31-1.fna.fbcdn.net&_nc_ohc=1WD_6uR4-YQAX8bNVJ9&oh=c1a39b9c3d874dcaf123ef6da35dd370&oe=5FAA6E6D" class="rounded-circle" style="height: 230px;">
+        </div>
+        <div class="col-9 p-4">
+            <div><h1>MD. Masudul Hasan Joarder</h1></div>
+            <div class="d-flex">
+                <div class="pr-3"><strong>54 </strong>Posts</div>
+                <div class="pr-3"><strong>244 </strong>Followers</div>
+                <div class="pr-3"><strong>397 </strong>Following</div>
             </div>
+
         </div>
     </div>
 </div>
